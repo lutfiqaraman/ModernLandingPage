bd0d4c7be09540872651dab5e9ce40fe3c600eb2
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-17 15:16:02 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "page-purple": "#5267DF",
+        "page-red"   : "#FA5959",
+        "page-blue"  : "#242A45",
+        "page-gray"  : "#9194A2",
+        "page-white" : "#F7F7F7",
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"]
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
